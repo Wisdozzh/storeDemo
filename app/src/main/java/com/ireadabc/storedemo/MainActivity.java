be99d816afb91aec9764303273d8af1ab8259477
赵智慧 <18304094954@163.com>
2020-08-09 13:44:22 +0800

    feat:家在fragment

--- app/src/main/java/com/ireadabc/storedemo/MainActivity.java
+++ app/src/main/java/com/ireadabc/storedemo/MainActivity.java
@@ -2,7 +2,14 @@ package com.ireadabc.storedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.ireadabc.storedemo.store.fragment.StoreFragment;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFragment();
+    }
+
+    private void initFragment() {
+        FragmentManager fm = this.getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragment, new StoreFragment());
+
+        // addToBackStack添加到回退栈,addToBackStack与ft.add(R.id.fragment, new
+        // MyFragment())效果相当
+        // ft.addToBackStack("test");
+
+        ft.commit();
+
     }
 }
--- app/src/main/java/com/ireadabc/storedemo/store/fragment/StoreFragment.java
+++ app/src/main/java/com/ireadabc/storedemo/store/fragment/StoreFragment.java
@@ -6,8 +6,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.ireadabc.storedemo.BaseFragment;
+import com.ireadabc.storedemo.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class StoreFragment extends Fragment {
 
+    Unbinder unbinder;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_store, container, false);
+//        unbinder = ButterKnife.bind(this, rootView);
+        initView();
+        initData();
+        return rootView;
+    }
+
+    private void initView() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private void initData() {
+    }
 }
+
